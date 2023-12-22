db9b3a3d8fce712c7e6a52f04ba057c36818864a
Wesley <devwesley@hotmail.com>
2023-12-22 11:22:33 -0300

    Segundo Exercicio do curso

---
+++ Segundo exercicio/Segundo_exercicio.js
@@ -0,0 +1,6 @@
+let number1 = prompt('Digite um numero');
+let number2 = prompt('Digite outro numero');
+number1 = parseFloat(number1);
+number2 = parseFloat(number2);
+const resultado = number1 + number2
+alert(`A soma dos numeros fornecidos são ${resultado}`);
